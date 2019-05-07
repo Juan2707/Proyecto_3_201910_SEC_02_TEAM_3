d7aa8b0f9737e1d78f1f75621ba96be33b0c0a43
unknown <ja.guzmanb1@uniandes.edu.co>
2019-05-06 21:44:54 -0500

    Diseno

--- test/test/GrafoTest.java
+++ test/test/GrafoTest.java
@@ -5,31 +5,16 @@ import model.estructuras.Graph;
 import model.estructuras.Vertice;
 
 public class GrafoTest<A, K, V> extends TestCase{
-	// -----------------------------------------------------------------
-	// Atributos
-	// -----------------------------------------------------------------
 
-	/**
-	 * Grafo sobre el que se van a hacer las pruebas.
-	 */
 	private Graph<Integer, Integer, Integer> grafo;
 
-	// -----------------------------------------------------------------
-	// Escenarios de prueba
-	// -----------------------------------------------------------------
-
-
-	/**
-	 * Crea un grafo con 5 vértices y 0 arcos
-	 */
 	public void setupEscenario1( )
 	{
-		// Crear el grafo vacio
 		grafo = new Graph<Integer, Integer, Integer>();
 
-		// Crear los vértices
 		try
-		{
+		{			
+			// TODO Auto-generated catch block
 			grafo.addVertex(1, 1);
 			grafo.addVertex(2, 2);
 			grafo.addVertex(3, 3);
@@ -38,25 +23,18 @@ public class GrafoTest<A, K, V> extends TestCase{
 		}
 		catch( Exception e )
 		{
-			// Esto no deberí a suceder
 			e.printStackTrace();
 			fail( );
 		}
 	}
 
-
-
-	/**
-	 * Crea un grafo con 5 vértices en forma de lista encadenada
-	 */
 	public void setupEscenario2( )
 	{
-		// Crear el grafo vacio
 		grafo = new Graph<Integer, Integer, Integer>();
 
-		// Crear los vértices
 		try
 		{
+			// TODO Auto-generated catch block
 			System.out.println(grafo.V());
 			grafo.addVertex(1, 1);
 			System.out.println(grafo.getInfoVertex(1)+",   " +grafo.V());
@@ -71,13 +49,12 @@ public class GrafoTest<A, K, V> extends TestCase{
 		}
 		catch( Exception e )
 		{
-			// Esto no debería suceder
 			fail( );
 		}
 
-		// Agregar los vértices
 		try
 		{
+			// TODO Auto-generated catch block
 			grafo.addEdge(1, 2, 1);
 			grafo.addEdge(2, 3, 2);
 			grafo.addEdge(3, 4, 3);
@@ -86,26 +63,17 @@ public class GrafoTest<A, K, V> extends TestCase{
 		}
 		catch( Exception e )
 		{
-			// Esto no deberí a
 			fail( );
 		}
 	}
 
-	
-	// -----------------------------------------------------------------
-	// Pruebas para métodos modificadores
-	// -----------------------------------------------------------------
-
-	/**
-	 * Pruebas de casos normales para el método <code>agregarVertice</code>
-	 */
 	public void testAgregar( )
 	{
-		// Crear el grafo vacio
 		setupEscenario1( );
 
 		try
 		{
+			// TODO Auto-generated catch block
 			grafo.addVertex(7, 7);
 			grafo.addEdge(7, 2, 7);
 			grafo.addEdge(7, 3, 8);
@@ -116,64 +84,33 @@ public class GrafoTest<A, K, V> extends TestCase{
 		}
 	}
 
-	/**
-	 * Pruebas de casos erroneos para el método <code>agregarVertice</code>
-	 */
 	public void testAgregarVerticeError( )
 	{
 		setupEscenario2( );
 
-		// Ingresar un vertice repetido
 		try
 		{
 			grafo.addVertex(1, 1);
+			// TODO Auto-generated catch block
 			fail( "El vértice ingresado ya existe" );
 		}
 		catch( Exception e )
 		{
-			// Este es el comportamiento esperado
 		}
 	}
 
-	/**
-	 * Pruebas de casos erroneos para el método <code>agregarArco</code>
-	 */
-	public void testAgregarArcoError( )
+	public void testAgregarArcoError( ) 
 	{
-		// Crear un grafo con 5 vertices en forma de lista encadenada
 		setupEscenario2( );
 
-		// Ingresar un arco entre vertices inexistentes
-		try
-		{
+		try {
 			grafo.addEdge(2, 3, 2);
-			fail( "Uno de los vértices conectados por el arco no existe" );
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		catch( Exception e )
-		{
-			// Comportamiento esperado
-		}
-		
+		fail( "Uno de los vértices conectados por el arco no existe" );
+
+
 	}
-
-	
-	/**
-	 * Pruebas de casos normales para el método <code>hayCamino</code>
-	 */
-//	public void testHayCamino( )
-//	{
-//		setupEscenario2( );
-//
-//		try
-//		{
-//			assertTrue( "Algoritmo invalido para detección de caminos", grafo.hayCamino( 0, 3 ) );
-//			assertTrue( "Algoritmo invalido para detección de caminos", grafo.hayCamino( 0, 5 ) );
-//			assertFalse( "Algoritmo invalido para detección de caminos", grafo.hayCamino( 0, 0 ) );
-//		}
-//		catch( Exception e )
-//		{
-//			fail( "el vertice " + e.darIdentificador( ) + " si existe" );
-//		}
-//	}
-
 }
